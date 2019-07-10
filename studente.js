7beee92993f71030e7b2806985950a3343f70eef
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-10 15:58:56 +0200

    --creato array contenente diversi oggetti studenti -- --visualizzo nome e cognome di ogni studente--

--- studente.js
+++ studente.js
@@ -5,7 +5,30 @@ var studente = {
   nome :"mario",
   cognome : "rossi",
   eta : 20
-}
+};
 for (var x in studente) {
  console.log(x + ":" + studente[x]);
+}
+/*Creare un array di oggetti di studenti.
+Ciclare su tutti gli studenti e stampare per ognuno nome e cognome*/
+var studenti =[
+  {
+    nome:"mario",
+    cognome:"rossi",
+    eta:20
+  },
+  {
+    nome:"filippo",
+    cognome:"inzaghi",
+    eta:40
+  },
+  {
+    nome:"cesare",
+    cognome:"cremonini",
+    eta:35
+  }
+
+];
+for(var i =0;i<studenti.length;i++){
+  console.log(studenti[i].nome +"--" + studenti[i].cognome);
 }
